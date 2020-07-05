12d989a911d7df08c426b6b53654b3170791ca1b
AbduEth <abdurekmjob@gmail.com>
2020-07-05 10:53:27 -0400

    yes

---
+++ src/rresumeData.js
@@ -0,0 +1,97 @@
+let resumeData = {
+  "imagebaseurl":"https://abdurezakmussa.github.io/Devportfolio/",
+  "name": "Abdurezak Abdelkader",
+  "role": "Frontend Developer",
+  "linkedinId":"Your LinkedIn Id",
+  "skypeid": "Your skypeid",
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"http://github.com/",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"http://skype.com",
+        "className":"fa fa-skype"
+      }
+    ],
+  "aboutme":"Software Developer with more than 5 years of experience in integrating, designing, developing and implementing web application solutions. Proven ability to manage all phases of software development life cycle from requirements analysis to application deployment. Skilled in React, Azure, Express.js, npm, node.JS, web API, MVC, .Net core, Entity Framework, C#, .NET, T-SQL and Mongo with database analysis and design..",
+  "address":"USA",
+  "website":"https://abdurezakmussa.github.io/Devportfolio/",
+  "education":[
+    {
+      "UniversityName":"HilCoE School of computer science and technology",
+      "MonthOfPassing":"Jan",
+      "YearOfPassing":"2010",
+      "Achievements":"Bachelor of Science in Computer Science"
+    },
+    {
+      "UniversityName":"University of Richmond",
+      "MonthOfPassing":"Jan",
+      "YearOfPassing":"2020",
+      "Achievements":"Full Stack Development"
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"CYBERSOFT PLC",
+      "MonthOfLeaving":"JJune ",
+      "YearOfLeaving":"2010",
+      "Achievements":"Participated in Ministry of trade: Trade management system including online trade registration Renewal and cancelation throughout all Ethiopian regions."
+    },
+    {
+      "CompanyName":"Mastec Advanced Technology",
+      "MonthOfLeaving":"August ",
+      "YearOfLeaving":"2015",
+      "Achievements":"database administration "
+    }
+  ],
+  "skillsDescription":"Your skills here",
+  "skills":[
+    {
+      "skillname":"MySql"
+    },
+    {
+      "skillname":"Nodejs"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"Cocktail-Mix",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"project2",
+      "description":"Curb Side online food order",
+      "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+      "name":"Restaurant",
+      "description":"Restaurant website",
+      "imgurl":"images/portfolio/project2.png"
+    },
+     ],
+  "testimonials":[
+    {
+      "description":"Software Ingineering",
+      "name":"Some technical guy"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
